145817738c332b9a297b7a379fb3981e32424983
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-21 21:37:28 +0900

    Home page 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,11 @@ const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
     return (
-        <main>
+        <main className="w-screen h-screen">
             <title> Home Page </title>
             <section>
-                <div className="">
-                    <h1> Welcome to DevBlog </h1>
+                <div className="mt-3 text-center">
+                    <h1 className="text-[3rem]"> Welcome to DevBlog </h1>
                     <p> A blog made with NextJS, TailwindCSS, Github GraphQL </p>
                 </div>
             </section>
